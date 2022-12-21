388f668c74a57ff9f7ba86b07c54b9f8aa936ae9
phongpnh <phongpnh@cozwork.com>
2022-12-21 21:28:30 +0700

    done

--- client/src/helpers/axios.js
+++ client/src/helpers/axios.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const axiosIntance = axios.create({
-    baseURL: ' http://localhost:4000/api/v1/',
+    baseURL: 'https://memories-server-ybd9.onrender.com/api/v1/',
 });
 
 axiosIntance.interceptors.request.use((req) => {
-    if(localStorage.getItem('profile')){
+    if (localStorage.getItem('profile')) {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
-    
+
     return req;
 })
 
